6b8590a45234aff00b913e229b3e50cee0f92550
tily <tily05@gmail.com>
2014-07-13 18:26:19 +0900

    add messenger module

---
+++ lib/domoraen/messenger.rb
@@ -0,0 +1,13 @@
+module Domoraen::Messenger
+	def sqs
+		@sqs ||= AWS::SQS.new(
+			sqs_endpoint: config[:messaenger][:sqs_endpoint],
+			access_key_id: config[:messenger][:access_key_id],
+			secret_access_key: config[:messenger][:secret_access_key]
+		)
+	end
+
+	def queue
+		@queue ||= sqs.queues.create(config[:messenger][:queue_name])
+	end
+end
