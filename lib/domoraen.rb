03c503570a766382f7c55431d7ae85f29adfb7b4
tily <tily05@gmail.com>
2014-07-13 12:06:12 +0900

    declare OpenSSL::SSL::VERIFY_PEER is OpenSSL::SSL::VERIFY_NONE

--- lib/domoraen.rb
+++ lib/domoraen.rb
@@ -1,5 +1,8 @@
 # coding:utf-8
 
+require 'openssl'
+OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
+
 class Domoraen
 	VERSION = File.read File.join File.dirname(__FILE__), '../VERSION'
 
@@ -8,12 +11,33 @@ class Domoraen
 	class << self
 		attr_accessor :env
 	end
+
+	def self.start
+		@domoraen = Domoraen::Bot.new(
+			config_file: File.dirname(__FILE__) + '/../conf/domoraen.yaml'
+		)
+		# TODO: update profile with current version
+
+		loop do
+			if text = @domoraen.produce_tool
+				@domoraen.status(text)
+			end
+
+			@domoraen.replies.each do |tweet|
+				text = @domoraen.react_to(tweet)
+				@domoraen.reply(text, tweet)
+			end
+
+			@domoraen.update_config
+
+			sleep SLEEP
+		end
+	end
 end
 
 require 'chatterbot'
 #require 'domoraen/config'
 #require 'domoraen/logger'
-require 'domoraen/dsl'
 require 'domoraen/producer'
 require 'domoraen/messenger'
 require 'domoraen/markov'
