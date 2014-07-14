182554d2fb827aac481f30d8a20ff957d8d19371
tily <tily05@gmail.com>
2014-07-15 00:04:44 +0900

    add Domoraen::Messenger module

--- lib/domoraen/messenger.rb
+++ lib/domoraen/messenger.rb
@@ -1,13 +1,15 @@
+require 'aws-sdk'
+
 module Domoraen::Messenger
 	def sqs
-		@sqs ||= AWS::SQS.new(
-			sqs_endpoint: config[:messaenger][:sqs_endpoint],
-			access_key_id: config[:messenger][:access_key_id],
-			secret_access_key: config[:messenger][:secret_access_key]
+		@sqs ||= ::AWS::SQS.new(
+			sqs_endpoint: @config[:messenger][:sqs_endpoint],
+			access_key_id: @config[:messenger][:access_key_id],
+			secret_access_key: @config[:messenger][:secret_access_key]
 		)
 	end
 
 	def queue
-		@queue ||= sqs.queues.create(config[:messenger][:queue_name])
+		@queue ||= sqs.queues.create(@config[:messenger][:queue_name])
 	end
 end
